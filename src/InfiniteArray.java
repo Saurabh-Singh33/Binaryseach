dfcf78da0e261483f69d2fff64f0f19aa78ac3c4
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2025-08-15 16:24:05 +0530

    InfiniteArray Question

---
+++ src/InfiniteArray.java
@@ -0,0 +1,39 @@
+public class InfiniteArray {
+public static void main(String[] args) {
+    int [] arr =  { 3,5,7,9,10,90,100,130,140,160,170};
+    int target = 10 ;
+    System.out.println(ans(arr,target));
+}
+static int ans(int [] arr , int target) {
+    // first find the range
+    // start with box of 2
+    int start = 0;
+    int end = 1;
+    //condition of target lie in the range
+    while (target > arr[end]) {
+        int newStart = end + 1;
+        //double the box value
+        //end = previous end + sizeofbox *2;
+        end = end + (end - start + 1) * 2;
+            start = newStart;
+    }
+   return Binarysearch(  arr ,  target , start, end);
+}
+static int Binarysearch(int [] arr , int target ,int start, int end){
+    while(start<=end){
+        // to find the middle element
+        int mid = start + (end-start)/2;
+        if(target < arr[mid]){
+            end = mid -1;
+
+        } else if (target> arr[mid]) {
+            start = mid +1;
+        }
+        else{
+            // ans found
+            return mid;
+    }
+    }
+    return -1;
+}
+}
