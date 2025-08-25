1162f8adc3b54e7eae3c512b2fd8e61a68d1efdf
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2025-08-25 13:41:30 +0530

    Selection Sorting

---
+++ src/SelectionSorting.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class SelectionSorting {
+    public static void main(String[] args) {
+        int [] arr = {1, 4, 6, 4, 7};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[]arr){
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr,0 , last);
+            swap (arr , maxIndex , last);
+
+        }
+    }
+static void swap(int []arr , int first , int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second] = temp;
+}
+     static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start ;
+        for (int i = 0; i <=end; i++) {
+            if(arr[max]< arr[i]){
+                max =i;
+            }
+        }
+        return max;
+    }
+
+    static void bubble(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
