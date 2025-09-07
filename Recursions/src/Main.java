d9ba28712f17cfe5460da42972a2eb92856b0dd6
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2025-09-07 21:54:17 +0530

    recursion initials

--- Recursions/src/Main.java
+++ Recursions/src/Main.java
@@ -1,6 +1,9 @@
+
+
 public class Main {
+
     // Recursive method to calculate factorial
-    public static int factorial(int n) {
+   /* public static int factorial(int n) {
         if (n == 0) {  // Base case
             return 1;
         } else {       // Recursive call
@@ -12,5 +15,62 @@ public class Main {
         int num = 5;  // Example number
         int result = factorial(num);
         System.out.println("Factorial of " + num + " is " + result);
+    }*/
+
+
+    // message
+   /* public static void main(String[] args) {
+        Message();
     }
+    static void Message(){
+        System.out.println("Hello World");
+        Message1();
+    }
+    static void Message1(){
+        System.out.println("Hello World");
+        Message2();
+    }
+    static void Message2(){
+        System.out.println("Hello World");
+        Message3();
+    }
+    static void Message3(){
+        System.out.println("Hello World");
+        Message4();
+    }
+    static void Message4(){
+        System.out.println("Hello World");
+    }*/
+
+    // For counting numbers
+    public static void main(String[] args) {
+
+
+        print1(1);
+    }
+
+static void print1(int n) {
+    System.out.println(n);
+     print2(2);
+}
+
+static void print2(int n) {
+    System.out.println(n);
+     print3(3);
+}
+
+static void print3(int n) {
+    System.out.println(n);
+     print4(4);
+}
+
+static void print4(int n) {
+    System.out.println(n);
+     print5(5);
+}
+
+static void print5(int n) {
+    System.out.println(n);
+
+}
 }
